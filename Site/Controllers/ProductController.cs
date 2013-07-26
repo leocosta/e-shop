4a242fd5d0766cb517761628a165f36947174933
RenanCabral <renan.baptista.cabral@gmail.com>
2013-07-26 12:18:00 -0300

    Criacao da interface inicial, de listagem de categorias e produtos

---
+++ Site/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class ProductController : Controller
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult List(String id)
+        {
+            return View();
+        }
+
+    }
+}
